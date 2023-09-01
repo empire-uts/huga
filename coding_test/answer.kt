536af322af95d9a46f73d6e91d809f13b65afb81
empire-uts <kii.einsatz@docomo.ne.jp>
2023-09-01 19:06:19 +0900

    comment add

--- coding_test/answer.kt
+++ coding_test/answer.kt
@@ -1,26 +1,41 @@
 import kotlin.math.abs
 
 fun main(args: Array<String>) {
+    // 入力をキャッチ
     val N = readLine()!!.toInt()
 
+    // 一行ずつ出力していく
     for (i in 1..N) {
+        // 手前のスペースを計算
         val spaces = N - i
+
+        // 奥の数字を、切り分けた関数で計算
         val numbers = generateNumbers(i)
+        
+        // つなげて出力
         val row = "${" ".repeat(spaces)}$numbers"
         println(row)
     }
 }
 
+// 切り分けた関数
 fun generateNumbers(n: Int): String {
+    // 便利なクラス 
+    // appendを重ねる事ができるとかなんとか しらんけど
+    // 注意点として、最後に要toString()
     val sb = StringBuilder()
 
+    // 登って、
     for (i in 1..n) {
         sb.append(i % 10)
     }
 
+    // 下る
+        // downTo 、なんか便利そう（こなみ）
     for (i in n - 1 downTo 1) {
         sb.append(i % 10)
     }
 
+    // 最後につなげて出力
     return sb.toString()
 }
